1c30fa3019760e87956795cfc5a093bdb951794f
Eric D Helms <ericdhelms@gmail.com>
2011-10-04 22:41:06 -0400

    Adds basic views for file uploading.

--- mediscan/tests/view_tests.py
+++ mediscan/tests/view_tests.py
@@ -8,7 +8,19 @@ class ViewTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
+    
+    def test_get_image_upload(self):
+        response = self.app.get('/image/upload/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_get(self):
-        response = self.app.get('/')
+    def test_get_images(self):
+        response = self.app.get('/image/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_put_image(self):
+        response = self.app.put('/image/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_image(self):
+        response = self.app.get('/image/376/')
         self.assertEqual(response.status_code, 200)
--- mediscan/views.py
+++ mediscan/views.py
@@ -1,8 +1,39 @@
-from flask import Flask
+import os
 
+from flask import Flask, request, make_response, render_template
+
+from werkzeug import secure_filename
+
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/media/'
+ALLOWED_EXTENSIONS = set(['txt'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def images():
-    return "Images!"
+def allowed_files(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/image/upload/', methods=['GET'])
+def upload_image():
+    if request.method == 'GET':
+        return render_template('templates/upload.html')
+
+@app.route('/image/', methods=['GET', 'PUT'])
+def show_images():
+    if request.method == 'GET':
+        resp = make_response(200)
+        resp.data = "Images!"
+        return render_template('templates/images.html')
+    elif request.method == 'PUT':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'Sucessfully uploaded file'
+
+
+@app.route('/image/<image_id>/')
+def image(image_id):
+    return image_id
