560cae96bcc86e1d9c67678580f1cf779c7f5787
Eric D Helms <ericdhelms@gmail.com>
2011-10-18 19:55:58 -0400

    Changes create virtualenv script to python and changes it to load pip and easy_install requirements into the virtual env. Changes requirements to be json.

---
+++ scripts/create-virtualenv.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+import simplejson
+from subprocess import call
+
+
+ENV_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../'
+os.chdir(ENV_DIR)
+
+print("Step 1: Removing old environment.")
+call('rm -rf medipa-env', shell=True)
+print("Old environment successfully removed.")
+
+print("Step 2: Creating virtual env.")
+call('virtualenv --no-site-packages --distribute medipa-env', shell=True)
+print("Virutal environment successfully installed.")
+
+print("Step 3: Loading requirements files.")
+requirements = open('requirements.json').read()
+requirements = simplejson.loads(requirements)
+print("Requirements file loaded.")
+
+print("Step 4: Installing pip requirements.")
+if 'pip' in requirements:
+    for req in requirements['pip']:
+        call("pip install " + req + " -E medipa-env", shell=True)
+    print("Pip packages successfully installed")
+else:
+    print("No pip packages specified.")
+
+print("Step 5: Installing easy_install requirements.")
+if 'easy_install' in requirements:
+    for req in requirements['easy_install']:
+        call("medipa-env/bin/easy_install " + req, shell=True)
+    print("easy_install packages successfully installed.")
+else:
+    print("No easy_install packages specified")
+print("Fin")
--- scripts/create-virtualenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf medipa-env
-virtualenv --no-site-packages --distribute medipa-env
-for line in $(cat requirements.txt)
-do
-  pip install $line -E medipa-env
-done
