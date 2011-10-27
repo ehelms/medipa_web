a1f61bf4254f119355dc5df22a078569ba84848e
Eric D Helms <ericdhelms@gmail.com>
2011-10-27 19:28:33 -0400

    Changes to setup for openshift.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='Medipa',
       description='Visualization of medical scans in WebGL.',
       author='Eric D Helms, David Gao, Justin Sherrill, Juhee Bae',
       url='http://www.python.org/sigs/distutils-sig/',
-      scripts=['scripts/create-virtualenv.py']
+      scripts=['../scripts/create-virtualenv.py']
      )
