bfeb7b8d9f384f5c27c401e0d3abd6ef014c152c
Eric D Helms <ericdhelms@gmail.com>
2011-11-07 20:59:29 -0500

    Modifications to start and stop celeryd on wsgi application start.

--- wsgi/celeryconfig.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BROKER_TRANSPORT = "sqlakombu.transport.Transport"
-BROKER_HOST = "sqlite:///" + os.environ['OPENSHIFT_DATA_DIR'] + "/db/celerydb.sqlite"
-
-CELERY_RESULT_BACKEND = "database"
-CELERY_RESULT_DBURI = "sqlite:///" + os.environ['OPENSHIFT_DATA_DIR'] + "/db/celerydb.sqlite"
-
-CELERY_IMPORTS = ("medipa.tasks", )
