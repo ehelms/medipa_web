9c991385a6bbe427f23956aa9e80ec48b5be97a0
Eric D Helms <ericdhelms@gmail.com>
2011-11-28 17:16:41 -0500

    Can now specify manage.py generate-test-data --file= with a filename that has been placed in the data directory and generate png data.

--- medipa/manage.py
+++ medipa/manage.py
@@ -62,7 +62,10 @@ def start():
         test_runner.run()
 
     if args.action == "generate-test-data":
-        image_handler.process_file("test_file_upload.mha")
+        if args.file:
+            image_handler.process_file(args.file)
+        else:
+            image_handler.process_file("test_file_upload.mha")
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -74,7 +77,9 @@ def parse_args():
     parser_runserver.add_argument('--nodebug', action='store_false', dest='debug', help='Whether to turn debug on or off (default: true)')
 
     parser_test = subparsers.add_parser('test', help='Runs the test suite')
-    parser_test = subparsers.add_parser('generate-test-data', help='Runs the test suite')
+    parser_test_data = subparsers.add_parser('generate-test-data', help='Generates test data for a given file placed in the data directory.')
+    parser_test_data.add_argument('--file', nargs='?',
+                        help='Filname to process')
     
     return parser.parse_args()
 
