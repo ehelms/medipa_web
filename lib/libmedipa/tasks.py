6092e87bfff4c9d35f249241089d735b93d04ac6
Eric D Helms <ericdhelms@gmail.com>
2011-11-30 17:05:16 -0500

    Removes name from task

--- lib/libmedipa/tasks.py
+++ lib/libmedipa/tasks.py
@@ -3,6 +3,6 @@ from celery.task import task
 from libmedipa import image_handler
 
 
-@task(name="libmedipa.process_file")
+@task
 def process_file(filename):
     image_handler.process_file(filename)
