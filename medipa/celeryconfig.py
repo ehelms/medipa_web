1022d6327396c05c444355e00ade0643f0f4312f
Eric D Helms <ericdhelms@gmail.com>
2011-11-30 15:06:35 -0500

    changes toc elery config.

--- medipa/celeryconfig.py
+++ medipa/celeryconfig.py
@@ -1,7 +1,5 @@
-import os
-
 BROKER_TRANSPORT = "sqlakombu.transport.Transport"
-BROKER_HOST = "sqlite:///" + os.environ['MEDIPA_DATA_DIR'] + "/db/celerydb.sqlite"
+BROKER_HOST = "sqlite:///../data/db/celerydb.sqlite"
 
 CELERY_RESULT_BACKEND = "database"
-CELERY_RESULT_DBURI = "sqlite:///" + os.environ['MEDIPA_DATA_DIR'] + "/db/celerydb.sqlite"
+CELERY_RESULT_DBURI = "sqlite:///../data/db/celerydb.sqlite"
